4368cb783e1675af1eb683c1d2e8751ce85e47a7
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-09-16 15:31:22 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
+}
+
+export default App;
